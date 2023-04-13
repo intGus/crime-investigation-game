ffc723ce4e49278b3a5f7f114451d788e96b514a
intgus <46197948+intGus@users.noreply.github.com>
2023-04-12 23:11:29 -0400

    use a simple graph for travel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const arr = ['Venezuela', 'Mexico', 'Colombia', 'Peru', 'Chile', 'Argentina', 'Holanda', 'Bolivia', 'Uruguay', 'Sudafrica', 'Ecuador', 'Brasil', 'Canada', 'Dinamarca', 'Noruega', 'Australia', 'Japon', 'China', 'India', 'Pakistan', 'Espana', 'Portugal']
+  let len = arr.length,
+      taken = new Array(len);
 
-
-  function getRandom(arr, n) {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
+  function getRandom(n) {
+    var result = new Array(n)
     if (n > len)
         throw new RangeError("getRandom: more elements taken than available");
     while (n--) {
@@ -16,21 +16,49 @@ function App() {
         taken[x] = --len in taken ? taken[len] : len;
     }
     return result;
-}
+  }
 
-  const [data, setData] = useState(null);
+  const [graph, setGraph] = useState({
+    base: getRandom(3)
+  });
+  
+  // useEffect(() => {
+  //   fetch('https://restcountries.com/v3.1/all?fields=name,capital')
+  //     .then(response => response.json())
+  //     .then(json => setData(getRandom(json, 3)))
+  //     .catch(error => console.error(error));
+  // }, []);
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all?fields=name,capital')
-      .then(response => response.json())
-      .then(json => setData(getRandom(json, 3)))
-      .catch(error => console.error(error));
-  }, []);
+  const [current, setCurrent] = useState('base')
+  const [output, setOutput] = useState(graph[current])
+  
+  function travel(node) {
+    if (!graph[current].includes(node)) {
+      console.log('you can only travel to ' + graph[current])
+      return
+    }
+    if (node in graph) {
+      console.log(node + " exists")
+    } else {
+      setGraph((prevGraph) => {
+        return({
+          ...prevGraph,
+          [node]: [current, ...['prueba1','prueba2','prueba3']]
+        })
+        
+      })
+    }
+
+  setCurrent(node)
+  // setOutput(graph[node].map(index => index === 'base' ? 'Base' : index))
+  console.log('you can travel to: ' + output)
+  }
+
 
   let countries = 'loading'
-  if (data) {
-    countries = data.map((item, index) =>
-    <li key={index}>{item.name.official}</li>
+  if (graph) {
+    countries = graph[current].map((item, index) =>
+    <li key={index} onClick={()=>travel(item)}>{item}</li>
     )
   }
   
